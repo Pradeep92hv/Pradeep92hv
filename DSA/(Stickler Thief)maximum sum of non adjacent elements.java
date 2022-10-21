7ae6eb5aa4763fd943ce3e1dedf31c45d28bac4c
Pradeep  H V <97173194+Pradeep92hv@users.noreply.github.com>
2022-10-21 12:18:47 +0530

    Create (Stickler Thief)maximum sum of non adjacent elements.java

---
+++ DSA/(Stickler Thief)maximum sum of non adjacent elements.java
@@ -0,0 +1,28 @@
+
+
+
+https://practice.geeksforgeeks.org/problems/stickler-theif-1587115621/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+
+
+class Solution
+{
+    
+    static int solve(int []a, int n, int []dp)
+    {
+        if(n==0)return a[n];
+        if(n<0)return 0;
+        if(dp[n]!=-1)return dp[n];
+        int pick=a[n]+solve(a,n-2,dp);
+        int notpick=0+solve(a,n-1,dp);
+        
+        return dp[n]=Math.max(pick,notpick);
+    }
+    //Function to find the maximum money the thief can get.
+    public int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        int dp[]=new int[n];
+        Arrays.fill(dp,-1);
+        return solve(arr,n-1,dp);
+    }
+}
