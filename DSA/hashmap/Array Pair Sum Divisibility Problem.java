f4dada89b893933c2f512f0d83ccc39d85a06c9b
Pradeep  H V <97173194+Pradeep92hv@users.noreply.github.com>
2022-10-22 23:08:24 +0530

    Create Array Pair Sum Divisibility Problem.java

---
+++ DSA/hashmap/Array Pair Sum Divisibility Problem.java
@@ -0,0 +1,42 @@
+https://practice.geeksforgeeks.org/problems/array-pair-sum-divisibility-problem3257/0
+
+class Solution {
+    public boolean canPair(int[] a, int k) {
+        // Code here
+         HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+         
+         for(int i=0;i<a.length;i++)
+         {
+             int rem=a[i]%k;
+             map.put(rem,map.getOrDefault(rem,0)+1);
+         }
+       
+          for(int i=0;i<a.length;i++)
+          {
+                 int rem=a[i]%k;
+
+            
+               if(rem==0 && map.get(rem)%2==1)
+               {
+                   
+                    return false;
+               }
+               
+               else if(rem*2==k && map.get(rem)%2==1)
+               {
+                   
+                    return false;
+               }
+            
+               else
+               {
+                   if(rem!=0 && map.get(rem)!=map.get(k-rem))
+                    return false;
+                   
+               }
+               
+              
+          }
+          return true;
+    }
+}
