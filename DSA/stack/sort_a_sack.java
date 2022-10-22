87d81166762f6c580b566efcf5103229290fdb7f
Pradeep  H V <97173194+Pradeep92hv@users.noreply.github.com>
2022-10-22 22:31:55 +0530

    Create sort_a_sack.java

---
+++ DSA/stack/sort_a_sack.java
@@ -0,0 +1,27 @@
+https://practice.geeksforgeeks.org/problems/sort-a-stack/0
+
+
+class GfG{
+    
+    static void fun(Stack<Integer> s,Stack<Integer> s1)
+    {
+        while(!s.isEmpty())
+        {
+            int a=s.pop();
+            
+            while(!s1.isEmpty() && s1.peek()>a)
+            {
+                s.push(s1.pop());
+            }
+            s1.push(a);
+        }
+    }
+	public Stack<Integer> sort(Stack<Integer> s)
+	{
+		//add code here.
+		Stack<Integer> s11=new Stack<Integer>();
+		
+		fun(s,s11);
+		return s11;
+	}
+}
