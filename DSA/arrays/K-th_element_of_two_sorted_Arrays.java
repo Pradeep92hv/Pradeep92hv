031c10c01ce1900f0685ac188dd219590384a5df
Pradeep  H V <97173194+Pradeep92hv@users.noreply.github.com>
2022-10-23 12:30:16 +0530

    Create K-th_element_of_two_sorted_Arrays.java

---
+++ DSA/arrays/K-th_element_of_two_sorted_Arrays.java
@@ -0,0 +1,28 @@
+
+https://practice.geeksforgeeks.org/problems/k-th-element-of-two-sorted-array1317/0
+
+class Solution {
+    public long kthElement( int a[], int b[], int n, int m, int kk) {
+       
+       int i=0,j=0,k=0;
+       int r[]=new int[n+m];
+       while(i<n && j<m)
+       {
+           if(a[i]>b[j])
+            r[k++]=b[j++];
+            else if(a[i]<b[j])
+              r[k++]=a[i++];
+              else
+              {
+                  r[k++]=a[i++];
+                  r[k++]=b[j++];
+              }
+       }
+       while(i<n)
+       r[k++]=a[i++];
+       while(j<m)
+       r[k++]=b[j++];
+       
+       return r[kk-1];
+    }
+}
